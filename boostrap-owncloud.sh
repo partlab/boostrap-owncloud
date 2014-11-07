a683ab57cf046242194b7e2edcb0856d2daeb6c9
Régis Gaidot <rgaidot@gmail.com>
2014-11-07 18:09:12 +0100

    fix  tar: Unrecognized archive format  tar: Error exit delayed from previous errors.

--- boostrap-owncloud.sh
+++ boostrap-owncloud.sh
@@ -7,5 +7,5 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-curl https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2 | tar -xvzf -
+curl -L https://download.owncloud.org/community/owncloud-7.0.2.tar.bz2 | tar -xvzf -
 mv owncloud $1 ; cp config/* $1 ; cd $1 ; fig up
